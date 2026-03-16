b0ab0e369236b270c393aa87a59bb2e1d21b55ca
Robin Banks <umair.almahmood10@gmail.com>
2026-03-16 16:36:30 -0400

    March 16

---
+++ Mar 16 (Switch Statements and Ternary Operator)/main.js
@@ -0,0 +1,53 @@
+
+
+let switchValue = new Date().getDay();
+
+switch (switchValue) {
+    case 0:
+        console.log('Sunday');
+        break;
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursday');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    case 6:
+        console.log('Saturday');
+        break;
+    default:
+        console.log('The eighth day the Beatles sang about');
+}
+
+
+// let condition = true;
+
+// let result = (condition) ? 'Condition is true' : 'Condition is false';
+
+// console.log(result);
+
+
+function isMult4(num) {
+    return false
+}
+
+function isCentury(year) {
+    return false
+}
+
+function isMult400(year) {
+    return false
+}
+
+function isLeapYear(year) {
+    return false
+}
