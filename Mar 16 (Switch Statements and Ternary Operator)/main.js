0d5383cb42e03ba09ff9dff90a586db7bf6475f3
Robin Banks <umair.almahmood10@gmail.com>
2026-03-16 18:06:24 -0400

    March 16 Final

--- Mar 16 (Switch Statements and Ternary Operator)/main.js
+++ Mar 16 (Switch Statements and Ternary Operator)/main.js
@@ -1,53 +1,49 @@
 
 
-let switchValue = new Date().getDay();
 
-switch (switchValue) {
-    case 0:
-        console.log('Sunday');
-        break;
-    case 1:
-        console.log('Monday');
-        break;
-    case 2:
-        console.log('Tuesday');
-        break;
-    case 3:
-        console.log('Wednesday');
-        break;
-    case 4:
-        console.log('Thursday');
-        break;
-    case 5:
-        console.log('Friday');
-        break;
-    case 6:
-        console.log('Saturday');
-        break;
-    default:
-        console.log('The eighth day the Beatles sang about');
-}
+// let switchValue = new Date().getDay();
 
-
-// let condition = true;
-
-// let result = (condition) ? 'Condition is true' : 'Condition is false';
-
-// console.log(result);
+// switch (switchValue) {
+//     case 0:
+//         console.log('Sunday');
+//         break;
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thursday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;
+//     default:
+//         console.log('The eighth day the Beatles sang about');
+// }
 
 
-function isMult4(num) {
-    return false
+function isMult4(year) {
+    return year % 4 === 0;
 }
 
 function isCentury(year) {
-    return false
+    return year % 100 === 0;
 }
 
 function isMult400(year) {
-    return false
+    return year % 400 === 0;
 }
 
 function isLeapYear(year) {
-    return false
+    return isCentury(year) ? isMult400(year) : isMult4(year);
 }
+
+console.log(isLeapYear(2400));
