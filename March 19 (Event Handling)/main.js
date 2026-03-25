e173ac401cdff9934ba56d6ce0964a8cfab68cbc
Robin Banks <umair.almahmood10@gmail.com>
2026-03-25 14:35:36 -0400

    March 19 Final

--- March 19 (Event Handling)/main.js
+++ March 19 (Event Handling)/main.js
@@ -1,8 +1,9 @@
 
 // Event listener
 const clickEventDemo = document.querySelector(".click-event-demo");
+const heading = document.querySelector("h1");
 clickEventDemo.addEventListener("click", function() {
-    alert("Button Clicked!");
+    alert("Button clicked");
 });
 
 // Bubbling
@@ -19,4 +20,21 @@ child.addEventListener('click', function() {
 // Recreate yesterday's example, 
 // where a paragraph tag matches its text to the input in a text field
 // Bonus Challenge: Make the paragraph replace all letter E's in the input with 3's
+const textInput = document.getElementById("textInput");
+const paragraph = document.querySelector(".challenge-paragraph");
+textInput.addEventListener("input" , function() {
+     paragraph.innerText = Eis3(textInput.value);
+});
 
+
+function Eis3(str) {
+    let output = ""
+    for (const char of str) {
+        if (char === "e" || char === "E") {
+            output += "3";
+        } else {      
+            output += char;
+        }
+    }
+    return output;
+}
