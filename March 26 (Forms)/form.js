1fd7e49e0baa1aec248521edcf87848b6b3e43ee
Robin Banks <umair.almahmood10@gmail.com>
2026-03-26 16:21:59 -0400

    March 26 Tidy

--- March 26 (Forms)/form.js
+++ March 26 (Forms)/form.js
@@ -1,7 +1,7 @@
 
 const form = document.querySelector("form");
-const template = document.getElementById("user-data-template");
-const userDataContainer = document.querySelector(".user-data-container");
+// const template = document.getElementById("user-data-template");
+// const userDataContainer = document.querySelector(".user-data-container");
 
 let userCount = 0;
 
@@ -20,20 +20,11 @@ form.addEventListener("submit", function(event) {
     // Do something with the JSON data (e.g., send it to a server)
     console.log(data);
 
-    const clone = template.content.cloneNode(true);
+    // const clone = template.content.cloneNode(true);
 
-    clone.querySelector("h3").textContent = `User #${userCount}`;
-    clone.querySelector(".user-name").textContent = `${capitalize(data.fname)} ${capitalize(data.lname)}`;
-    clone.querySelector(".user-email").textContent = data.email;
+    // clone.querySelector("h3").textContent = `User #${userCount}`;
+    // clone.querySelector(".user-name").textContent = `${capitalize(data.fname)} ${capitalize(data.lname)}`;
+    // clone.querySelector(".user-email").textContent = data.email;
 
-    userDataContainer.appendChild(clone);
+    // userDataContainer.appendChild(clone);
 });
-
-function capitalize(str) {
-    
-    if(!str) {
-        return "";
-    }
-
-    return str.charAt(0).toUpperCase() + str.substring(1);
-}
